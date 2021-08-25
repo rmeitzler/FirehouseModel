f5e9188a2286e32442d200d7fd5764a09cc2901b
Richard Meitzler <richard.meitzler@gmail.com>
2021-08-25 01:51:37 -0400

    model manager

--- Sources/FirehouseModel/ModelManager.swift
+++ Sources/FirehouseModel/ModelManager.swift
@@ -20,11 +20,11 @@ public class ModelManager: ObservableObject {
 
     subscriber = xmlManager.$treeData.sink { completion in
       switch completion {
-              case .finished:
-                  print("finished")
-              case .failure(let never):
-                  print(never)
-          }
+          case .finished:
+              print("finished")
+          case .failure(let never):
+              print(never)
+      }
     } receiveValue: { value in
       if let val = value {
           do {
@@ -39,7 +39,48 @@ public class ModelManager: ObservableObject {
           }
       }
     }
+  }
+  
+}
 
-    
+
+extension ModelManager {
+  
+  public func submenus(for menuId: String) -> [Submenu]? {
+    let submenuIds: [String]? = model?.menus.filter({$0.id == menuId}).first?.submenuIds.map({$0.id})
+    return model?.submenus.filter({ submenuIds?.contains($0.id) ?? false })
+  }
+  
+  public func menuItems(for submenuId: String) -> [MenuItem]? {
+    let menuitemIds: [String]? = model?.submenus.filter({$0.id == submenuId}).first?.menuItemIds.map({$0})
+    return model?.menuItems.filter({ menuitemIds?.contains($0.id) ?? false })
+  }
+  
+  public func salesItems(for menuItemId: String) -> [SalesItem]? {
+    return model?.salesItems.filter({$0.itemId == menuItemId})
+  }
+  
+  public func optionSet(for salesItemId: String) -> OptionSet? {
+    let optionSetId: String? = model?.salesItems.filter({$0.id == salesItemId}).first?.itemOptionSetId
+    return model?.optionSets.filter({$0.id == optionSetId ?? ""}).first
+  }
+  
+  public func optionGroups(for optionSetId: String) -> [OptionGroup]? {
+    let groupIds: [String]? = model?.optionSets.filter({$0.id == optionSetId}).first?.groups?.map({$0.id})
+    return groupIds?.compactMap({ optionGroup(for: $0) })
+  }
+  
+  public func optionGroup(for groupId: String) -> OptionGroup? {
+    return model?.optionGroups.filter({$0.id == groupId}).first
+  }
+  
+  public func salesItemOptions(for optionGroupId: String) -> [SalesItemOption]? {
+    let optionGroup: OptionGroup? = model?.optionGroups.filter({$0.id == optionGroupId}).first
+    let optionIds: [String]? = optionGroup?.options.map({$0.id})
+    return optionIds?.compactMap({ salesItemOption(for: $0) })
+  }
+  
+  public func salesItemOption(for optionId: String) -> SalesItemOption? {
+    return model?.salesItemOptions.filter({$0.id == optionId}).first
   }
 }
